e4a03c1d8f5cc32e22869019de97bc9adaa6b9d8
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-05-13 13:11:51 +0200

    Move licence to a licence file

--- hotp.py
+++ hotp.py
@@ -7,18 +7,7 @@ import calendar
 
 '''
 Python implementation of HOTP and TOTP algorithms from the OATH project.
-
-Copyright 2010, Benjamin Dauvergne
-
-* All rights reserved.
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.'''
+'''
 
 def __truncated_value(h):
     bytes = map(ord, h)
