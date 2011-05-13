130fc1c9af0404ffd7e653fa1d63f99dac932e4c
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-05-13 16:06:28 +0200

    Add test vectors from the draft RFC

--- oath/totp.py
+++ oath/totp.py
@@ -42,14 +42,81 @@ def accept_totp(key, response, period=30, format='dec8', hash=hashlib.sha1,
 if __name__ == '__main__':
     import binascii
     # Test vectors extracted from RFC 4226
-    secret = binascii.hexlify('12345678901234567890')
-    tvector3 = [
-            (59, hashlib.sha1, '94287082'),
-            (1111111109, hashlib.sha1, '07081804') ]
-    for timestamp, hash, value in tvector3:
-        assert (totp(secret,t=datetime.datetime.utcfromtimestamp(timestamp),hash=hash) == value)
-    assert(accept_totp(secret, '94287082', t=65) == (True, -1))
-    assert(accept_totp(secret, '94287082', t=65, drift=-1) == (True, -1))
-    import sys
-    if len(sys.argv) > 1:
-        print totp(sys.argv[1], format='dec6')
+    key_sha1 = binascii.hexlify('1234567890'*2)
+    key_sha256 = binascii.hexlify('1234567890'*2+'12')
+    key_sha512 = binascii.hexlify('1234567890'*6+'1234')
+    # Test vector extracted from
+    # http://tools.ietf.org/html/draft-mraihi-totp-timebased-08#appendix-B
+    tv = '''|      59     |  1970-01-01  | 0000000000000001 | 94287082 |  SHA1  |
+   |             |   00:00:59   |                  |          |        |
+   |      59     |  1970-01-01  | 0000000000000001 | 46119246 | SHA256 |
+   |             |   00:00:59   |                  |          |        |
+   |      59     |  1970-01-01  | 0000000000000001 | 90693936 | SHA512 |
+   |             |   00:00:59   |                  |          |        |
+   |  1111111109 |  2005-03-18  | 00000000023523EC | 07081804 |  SHA1  |
+   |             |   01:58:29   |                  |          |        |
+   |  1111111109 |  2005-03-18  | 00000000023523EC | 68084774 | SHA256 |
+   |             |   01:58:29   |                  |          |        |
+   |  1111111109 |  2005-03-18  | 00000000023523EC | 25091201 | SHA512 |
+   |             |   01:58:29   |                  |          |        |
+   |  1111111111 |  2005-03-18  | 00000000023523ED | 14050471 |  SHA1  |
+   |             |   01:58:31   |                  |          |        |
+   |  1111111111 |  2005-03-18  | 00000000023523ED | 67062674 | SHA256 |
+   |             |   01:58:31   |                  |          |        |
+   |  1111111111 |  2005-03-18  | 00000000023523ED | 99943326 | SHA512 |
+   |             |   01:58:31   |                  |          |        |
+   |  1234567890 |  2009-02-13  | 000000000273EF07 | 89005924 |  SHA1  |
+   |             |   23:31:30   |                  |          |        |
+   |  1234567890 |  2009-02-13  | 000000000273EF07 | 91819424 | SHA256 |
+   |             |   23:31:30   |                  |          |        |
+   |  1234567890 |  2009-02-13  | 000000000273EF07 | 93441116 | SHA512 |
+   |             |   23:31:30   |                  |          |        |
+   |  2000000000 |  2033-05-18  | 0000000003F940AA | 69279037 |  SHA1  |
+   |             |   03:33:20   |                  |          |        |
+   |  2000000000 |  2033-05-18  | 0000000003F940AA | 90698825 | SHA256 |
+   |             |   03:33:20   |                  |          |        |
+   |  2000000000 |  2033-05-18  | 0000000003F940AA | 38618901 | SHA512 |
+   |             |   03:33:20   |                  |          |        |
+   | 20000000000 |  2603-10-11  | 0000000027BC86AA | 65353130 |  SHA1  |
+   |             |   11:33:20   |                  |          |        |
+   | 20000000000 |  2603-10-11  | 0000000027BC86AA | 77737706 | SHA256 |
+   |             |   11:33:20   |                  |          |        |
+   | 20000000000 |  2603-10-11  | 0000000027BC86AA | 47863826 | SHA512 |
+   |             |   11:33:20   |                  |          |        |'''
+    test_vectors  = [ [ cell.strip() for cell in line.strip(' |').split('|') ] for line in tv.splitlines()]
+    test_vectors  = [ line for line in test_vectors if line[0] and len(line) > 3 ]
+    hash_algos = {
+            'SHA1': {
+                'key': key_sha1,
+                'alg': hashlib.sha1, },
+            'SHA256': {
+                'key': key_sha256,
+                'alg': hashlib.sha256, },
+            'SHA512': {
+                'key': key_sha512,
+                'alg': hashlib.sha512, },
+    }
+    for t, _, _, response, algo_key in test_vectors:
+        assert accept_totp(hash_algos[algo_key]['key'], response, t=int(t),
+                hash=hash_algos[algo_key]['alg'])
+
+
+#    test_vectors = [
+#            (key_sha1, 59, '94287082', hashlib.sha1),
+#            (key_sha256, 59, '46119246', hashlib.sha256),
+#            (key_sha512, 59, '90693936', hashlib.sha512),
+#            (key_sha1, 1111111109, '07081804', hashlib.sha1),
+#            (key_sha256, 1111111109, '68084774', hashlib.sha256),
+#            (key_sha512, 1111111109, '25091201', hashlib.sha512),
+#            (key_sha1, 1111111111, '14050471', hashlib.sha1),
+#            (key_sha256, 1111111111, '67062674', hashlib.sha256),
+#            (key_sha512, 1111111111, '99943326', hashlib.sha512),
+#            (key_sha1, 1234567890, '', hashlib.sha1),
+#            (key_sha256, 1234567890, '', hashlib.sha256),
+#            (key_sha512, 1234567890, '', hashlib.sha512),
+#    for key, t, response, hash in test_vectors:
+#        assert accept_totp(key, response, t=t, hash=hash)
+#
+#    import sys
+#    if len(sys.argv) > 1:
+#        print totp(sys.argv[1], format='dec6')
