01c9b378cbe5a93067d3b661b1ce050eb9653f2f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-07-15 14:34:00 +0200

    add a test command to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python
-from distutils.core import setup
+from distutils.core import setup, Command
 import oath
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin
+import os
+
+class TestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        testfiles = [ ]
+        for t in glob(pjoin(self._dir, 'tests', '*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    ['tests', splitext(basename(t))[0]])
+                )
+
+        tests = TestLoader().loadTestsFromNames(testfiles)
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
 
 setup(name='oath',
         version=oath.VERSION,
@@ -9,4 +37,5 @@ setup(name='oath',
         url='http://github.con/bdauvergne/python-oath.git',
         author='Benjamin Dauvergne',
         author_email='bdauvergne@entrouvert.com',
-        packages=['oath'])
+        packages=['oath'],
+        cmdclass={'test': TestCommand})
