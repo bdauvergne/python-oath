79aab3be450ba43a1c9001a1e00b4cde193fdf2e
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-01-31 22:31:05 +0100

    bump version to 1.2

--- oath/__init__.py
+++ oath/__init__.py
@@ -3,4 +3,4 @@ from _hotp import *
 from _ocra import *
 from google_authenticator import *
 
-__version__ = VERSION = '1.0'
+__version__ = VERSION = '1.2'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class TestCommand(Command):
         t.run(tests)
 
 setup(name='oath',
-        version='1.1',
+        version='1.2',
         license='MIT',
         description='Python implementation of the three main OATH specifications: HOTP, TOTP and OCRA',
         url='https://github.com/bdauvergne/python-oath',
