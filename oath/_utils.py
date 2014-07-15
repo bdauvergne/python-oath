5b8409df404a8f546e1b69e2fa0efb72c015740d
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-07-15 09:43:26 +0200

    add new module _utils with first function compare_digest

---
+++ oath/_utils.py
@@ -0,0 +1,7 @@
+def compare_digest(a, b):
+    if not isinstance(a, str) or not isinstance(b, str):
+        raise TypeError
+    if len(a) != len(b):
+        return False
+    return reduce(bool.__and__, map(lambda x: x[0] == x[1], zip(a, b)))
+
