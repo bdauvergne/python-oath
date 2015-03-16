a665c17a2a0d1baca060baf06fdcd28b45c39143
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2015-03-16 15:34:33 +0100

    Simplify running tests, and use setuptools instead of distribute

--- setup.py
+++ setup.py
@@ -1,33 +1,5 @@
 #!/usr/bin/python
-from distutils.core import setup, Command
-from unittest import TextTestRunner, TestLoader
-from glob import glob
-from os.path import splitext, basename, join as pjoin
-import os
-
-class TestCommand(Command):
-    user_options = [ ]
-
-    def initialize_options(self):
-        self._dir = os.getcwd()
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        '''
-        Finds all the tests modules in tests/, and runs them.
-        '''
-        testfiles = [ ]
-        for t in glob(pjoin(self._dir, 'tests', '*.py')):
-            if not t.endswith('__init__.py'):
-                testfiles.append('.'.join(
-                    ['tests', splitext(basename(t))[0]])
-                )
-
-        tests = TestLoader().loadTestsFromNames(testfiles)
-        t = TextTestRunner(verbosity = 4)
-        t.run(tests)
+from setuptools import setup
 
 setup(name='oath',
         version='1.2',
@@ -37,7 +9,7 @@ setup(name='oath',
         author='Benjamin Dauvergne',
         author_email='bdauvergne@entrouvert.com',
         packages=['oath'],
-        cmdclass={'test': TestCommand},
+        test_suite='tests',
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             'Intended Audience :: Developers',
