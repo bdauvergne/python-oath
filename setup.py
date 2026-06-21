fd9e07b2b10a088a7ae560cd3604d11e0891e8ff
Benson Muite <bkmgit@users.noreply.github.com>
2026-06-21 20:37:42 +0300

    Remove incorrect license tag
    OSI approved classifier has been deprecated from Python packaging guidelines.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         'Intended Audience :: Developers',
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
